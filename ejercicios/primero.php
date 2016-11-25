18ade5178daa542b546b4dc0966fcd4bd5c304fc
Thargelion <elwindwalker@gmail.com>
2016-11-24 22:15:45 -0300

    Sumado #100

--- ejercicios/primero.php
+++ ejercicios/primero.php
@@ -1,6 +1,6 @@
 <?php
 //Imprimir en pantalla los números del 1 al 100. Si el número es múltiplo de 3 que imprima pepe. Y si es múltiplo de 5: biondi
-for ($i=1; $i < 100; $i++) {
+for ($i=1; $i < 101; $i++) {
   $texto = $i;
   if($i % 3 == 0){
     $texto = $texto . " Pepe";
